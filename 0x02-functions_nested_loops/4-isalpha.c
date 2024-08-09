9fbde7a0fb9649af1a38bb634dc0c96bcf0aa4aa
Albert Anyika <disiluk15@gmail.com>
2024-08-09 16:53:14 -0700

    if_statements

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha - chack if c is alpha
+ * @c: char
+ * Return: 1 on success else 0
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'A' && c <= 'Z') |(c >= 'a' && c <= 'z'))
+		return (1);
+	return (0);
+}
