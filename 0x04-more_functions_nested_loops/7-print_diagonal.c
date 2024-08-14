2e804d8e986e4dcf169ef906b778327f5cd23712
Albert Anyika <disiluk15@gmail.com>
2024-08-14 11:15:41 -0700

    if_statements

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,14 +7,17 @@
 
 void print_diagonal(int n)
 {
-	int i;
+	int i, j;
 
 	if (n > 0)
 	{
-		for (i = 1; i <= n; i++)
-			_putchar(' ');
-		_putchar('\\');
-		_putchar('\n');
+		for (j = 0; j < n; j++)
+		{
+			for (i = n; i <= n; i--)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
 	}
 	else
 		_putchar('\n');
