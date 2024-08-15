32d8f091b28586b2b0907648fe5e94461b2c7dbc
Albert Anyika <disiluk15@gmail.com>
2024-08-14 18:33:39 -0700

    poinrter_and_arrays

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strlen - return len str
+ * @s: string
+ * Return: len of s
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * print_rev - print reverse str
+ * @s: string
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	i = _strlen(s);
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
