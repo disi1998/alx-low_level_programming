32766726406e01b83183890c97f61848ea136399
Albert Anyika <disiluk15@gmail.com>
2024-08-16 15:26:10 -0700

    poinrter_and_arrays

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half string
+ * @str: string
+ */
+
+void puts_half(char *str)
+{
+	int i = 0, j;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	if ((i - 1) % 2 == 0)
+	{
+		for (j = ((i - 1) / 2); j < i; j++)
+		{
+			_putchar(str[j]);
+		}
+	}
+	else
+		for (j = (i / 2); j < i; j++)
+			_putchar(str[j]);
+	_putchar('\n');
+}
