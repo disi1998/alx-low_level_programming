349fbeac9002420d63c7ec094a34169a8adb7fc1
Albert Anyika <disiluk15@gmail.com>
2024-08-16 16:43:57 -0700

    poinrter_and_arrays

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_array - print nums stored in array inorder
+ * @n: number of arrays
+ * @a: array
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (; i < (n - 1);)
+	{
+		printf("%d, ", a[i]);
+		i++;
+	}
+	printf("%d\n", a[n - 1]);
+}
