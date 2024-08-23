3e9c716fc725993c65aef200ce269050c250352a
Albert Anyika <disiluk15@gmail.com>
2024-08-23 16:50:51 -0700

    poinrter_and_arrays

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -13,7 +13,7 @@ void reverse_array(int *a, int n)
 	for (i = 0; i <= (n - 1) / 2; i++)
 	{
 		j = a[i];
-		a[i] = a[n - i - 1];
-		a[n - i - 1] = j;
+		a[i] = a[n - 1 - i];
+		a[n - 1 i] = j;
 	}
 }
